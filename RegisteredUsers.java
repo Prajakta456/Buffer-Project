83532c58f170327886d931396232135bbd4ed7e2
Prajakta <70521908+user12comp@users.noreply.github.com>
2021-06-05 12:54:47 +0530

    Create RegisteredUsers.java

---
+++ RegisteredUsers.java
@@ -0,0 +1,16 @@
+package tourism;
+//import java.util.Iterator;
+public class RegisteredUsers 
+{
+		   String cname;
+		   String password;
+		   char membership1;
+		   
+		  
+		  RegisteredUsers(String cname,String password,char membership1)
+		  {
+			this.membership1=membership1;  
+		    this.cname=cname;
+		    this.password=password;  
+		  }
+}
