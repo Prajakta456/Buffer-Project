9247558d6f245a749f6bf3f17fdf43445957d64f
Prajakta <70521908+user12comp@users.noreply.github.com>
2021-06-05 12:55:54 +0530

    Create CustomerComparator.java

---
+++ CustomerComparator.java
@@ -0,0 +1,31 @@
+package tourism;
+import java.util.*;
+public class CustomerComparator implements Comparator<Customer>
+	
+	 {
+        
+        //Overriding compare()method of Comparator 
+        //for descending order of priority
+                  
+   		@Override
+		public int compare(Customer c1, Customer c2) 
+   		{
+			// TODO Auto-generated method stub	
+	            if (c1.priority< c2.priority)
+	                return 1;
+	            else if (c1.priority > c2.priority)
+	                return -1;
+	   
+			return 0;
+		}
+	
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
