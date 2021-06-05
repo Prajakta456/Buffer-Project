05368e36b755769f16cea589e9f82242efc2ce79
Prajakta <70521908+user12comp@users.noreply.github.com>
2021-06-05 12:53:26 +0530

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,15 @@
+package tourism;
+public class Admin 
+{
+	   String adminName;
+	   String securityKey;
+	   
+	   Admin(String adminName,String securityKey)
+	  {
+	    this.adminName=adminName;
+	    this.securityKey=securityKey;     
+	  }
+	
+      
+}
+
