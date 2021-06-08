3ba817178fdcf4843d5d2c5148e906c1b31a3291
Prajakta <70521908+user12comp@users.noreply.github.com>
2021-06-08 21:34:55 +0530

    Create Accounts.java

---
+++ Accounts.java
@@ -0,0 +1,13 @@
+package tourism;
+
+public class Accounts 
+{
+   String acName;
+   double totalWithoutHotel;
+   double total;
+   Accounts(String n,double totalWithoutHotel,double total){
+	   this.acName=n;
+	   this.totalWithoutHotel=totalWithoutHotel;
+	   this.total=total;
+   }
+}
