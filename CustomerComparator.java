c0eb5e9abec7e589419d21a73cf579af30371df8
Prajakta Deokule <70521908+Prajakta456@users.noreply.github.com>
2021-08-29 22:37:09 +0530

    Update CustomerComparator.java

--- CustomerComparator.java
+++ CustomerComparator.java
@@ -7,6 +7,7 @@ public class CustomerComparator implements Comparator<Customer>
         //Overriding compare()method of Comparator 
         //for descending order of priority
                   
+	//when a positive value is returned by the compare method the items swap
    		@Override
 		public int compare(Customer c1, Customer c2) 
    		{	
